462a10f98e2b298f6768270c685cd67144e7be2e
Perry <samesense@users.noreply.github.com>
2017-06-26 16:16:52 -0400

    Update const.py

--- {{ cookiecutter.repo_name }}/src/rules/const.py
+++ {{ cookiecutter.repo_name }}/src/rules/const.py
@@ -4,6 +4,9 @@ SECRETS = '/home/evansj/me/.secrets/'
 sys.path.append(SECRETS)
 from pass_wd import *
 
+DONE = TWILIO_PRE + "--data-urlencode 'Body=DONE' " + TWILIO_POST
+FAIL = TWILIO_PRE + "--data-urlencode 'Body=FAIL' " + TWILIO_POST
+
 if 'code' in p:
     PWD = os.getcwd().split('src/')[0]
 else:
